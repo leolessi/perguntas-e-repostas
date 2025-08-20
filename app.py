0127e90ba8af54530a58483d052193746681d75b
leolessi <leonardolessi@gmail.com>
2025-08-19 23:32:44 -0300

    att app.py - estavel

--- app.py
+++ app.py
@@ -29,251 +29,251 @@ questions = [
         "Opções": ["Tupã", "Lua", "Titã", "Io"],
         "Resposta": "Lua",
     },
-    {
-        "Pergunta": "Qual é o único país onde o animal coala vive?",
-        "Opções": ["Brasil", "Estados Unidos", "Canadá", "Austrália"],
-        "Resposta": "Austrália",
-    },
-    {
-        "Pergunta": "As cores vermelho e azul misturadas formam qual cor?",
-        "Opções": ["Roxo", "Amarelo", "Verde", "Laranja"],
-        "Resposta": "Roxo",
-    },
-    {
-        "Pergunta": "Quantos anos tem um século?",
-        "Opções": ["100", "10", "1000", "50"],
-        "Resposta": "100",
-    },
-    {
-        "Pergunta": "Quem pintou o quadro Mona Lisa?",
-        "Opções": [
-            "Van Gogh",
-            "Osmar de Barros",
-            "Leonardo da Vinci",
-            "Leonardo DiCaprio",
-        ],
-        "Resposta": "Leonardo da Vinci",
-    },
-    {
-        "Pergunta": "Quem nasce em Pernambuco é o que?",
-        "Opções": ["Pernambuquês", "Pernambués", "Pernambucólico", "Pernambucano"],
-        "Resposta": "Pernambucano",
-    },
-    {
-        "Pergunta": "Que nome se dá para o coletivo de lobos?",
-        "Opções": ["Alcateia", "Cardume", "Vexame", "Matilha"],
-        "Resposta": "Alcateia",
-    },
-    {
-        "Pergunta": "Como é o nome da energia gerada pelo vento?",
-        "Opções": ["Hídrica", "Térmica", "Eólica", "Química"],
-        "Resposta": "Eólica",
-    },
-    {
-        "Pergunta": "Qual é o maior país da América do Sul?",
-        "Opções": ["Brasil", "Argentina", "Chile", "Colômbia"],
-        "Resposta": "Brasil",
-    },
-    {
-        "Pergunta": "Como é chamado o macho da cabra?",
-        "Opções": ["Cabrito", "Bode", "Carneiro", "Touro"],
-        "Resposta": "Bode",
-    },
-    {
-        "Pergunta": "Como é chamado o maior osso do corpo humano?",
-        "Opções": ["Fêmur", "Úmero", "Tíbia", "Costela"],
-        "Resposta": "Fêmur",
-    },
-    {
-        "Pergunta": "Qual é o sistema de governo adotado no Brasil?",
-        "Opções": [
-            "Monarquia parlamentarista",
-            "República presidencialista",
-            "Ditadura militar",
-            "República parlamentarista",
-        ],
-        "Resposta": "República presidencialista",
-    },
-    {
-        "Pergunta": "Qual é a floresta tropical mais extensa do mundo?",
-        "Opções": [
-            "Floresta Amazônica",
-            "Floresta Negra",
-            "Floresta de Sherwood",
-            "Floresta do Congo",
-        ],
-        "Resposta": "Floresta Amazônica",
-    },
-    {
-        "Pergunta": "Qual desses não é o nome de um oceano?",
-        "Opções": ["Polar", "Oceano Negro", "Pacífico", "Índico"],
-        "Resposta": "Oceano Negro",
-    },
-    {
-        "Pergunta": "Qual é a fórmula da água?",
-        "Opções": ["H2O", "CO2", "NaCl", "C6H12O6"],
-        "Resposta": "H2O",
-    },
-    {
-        "Pergunta": "Qual objeto costuma ser jogado pela noiva durante a festa de casamento?",
-        "Opções": ["Véu", "Sapato", "Aliança", "Buquê de flores"],
-        "Resposta": "Buquê de flores",
-    },
-    {
-        "Pergunta": "Qual é o nome do famoso navio que naufragou em 1912 e foi tema de um filme?",
-        "Opções": ["Queen Mary", "Enterprise", "Titanic", "Dear Rose"],
-        "Resposta": "Titanic",
-    },
-    {
-        "Pergunta": "Quantos dias tem um ano bissexto?",
-        "Opções": ["365", "366", "367", "364"],
-        "Resposta": "366",
-    },
-    {
-        "Pergunta": "Qual é o maior continente do mundo?",
-        "Opções": ["Europa", "América", "África", "Ásia"],
-        "Resposta": "Ásia",
-    },
-    {
-        "Pergunta": "Qual país tem como capital a cidade de Lisboa?",
-        "Opções": ["Espanha", "Portugal", "França", "Itália"],
-        "Resposta": "Portugal",
-    },
-    {
-        "Pergunta": "Quem é o responsável por buscar e devolver a bola ao campo durante um jogo de futebol?",
-        "Opções": ["Goleiro", "Zagueiro", "Atacante", "Gandula"],
-        "Resposta": "Gandula",
-    },
-    {
-        "Pergunta": "Você tem 36 laranjas e joga um terço fora, com quantas você fica?",
-        "Opções": ["6", "24", "12", "18"],
-        "Resposta": "24",
-    },
-    {
-        "Pergunta": "Quanto é 50% de 300?",
-        "Opções": ["25", "50", "100", "150"],
-        "Resposta": "150",
-    },
-    {
-        "Pergunta": "Qual é a capital da Espanha?",
-        "Opções": ["Valência", "Barcelona", "Sevilla", "Madrid"],
-        "Resposta": "Madrid",
-    },
-    {
-        "Pergunta": "Quantos séculos há em um milênio?",
-        "Opções": ["2 séculos", "10 séculos", "20 séculos", "15 séculos"],
-        "Resposta": "10 séculos",
-    },
-    {
-        "Pergunta": "Qual é o metal mais caro do mundo?",
-        "Opções": ["Ouro", "Platina", "Ródio", "Paládio"],
-        "Resposta": "Ródio",
-    },
-    {
-        "Pergunta": "Qual é a cor do sangue arterial?",
-        "Opções": ["Vermelho", "Azul", "Roxo", "Preto"],
-        "Resposta": "Vermelho",
-    },
-    {
-        "Pergunta": "Qual é o nome da haste de madeira de extremidade achatada usada para remar?",
-        "Opções": ["Vela", "Timão", "Remo", "Leme"],
-        "Resposta": "Remo",
-    },
-    {
-        "Pergunta": "Qual animal é conhecido como o 'rei da selva'?",
-        "Opções": ["Leopardo", "Tigre", "Leão", "Onça"],
-        "Resposta": "Leão",
-    },
-    {
-        "Pergunta": "Qual é o mineral capaz de enfraquecer o Super-Homem?",
-        "Opções": ["Ametista", "Quartzo", "Diamante", "Kriptonita"],
-        "Resposta": "Kriptonita",
-    },
-    {
-        "Pergunta": "Qual é o coletivo de lobos?",
-        "Opções": ["Matilha", "Bando", "Alcateia", "Manada"],
-        "Resposta": "Alcateia",
-    },
-    {
-        "Pergunta": "A moranga é uma variedade de:",
-        "Opções": ["Morango", "Abóbora", "Melancia", "Ameixa"],
-        "Resposta": "Abóbora",
-    },
-    {
-        "Pergunta": "Como é chamado um ângulo de 90º?",
-        "Opções": ["Reto", "Obtuso", "Agudo", "Raso"],
-        "Resposta": "Reto",
-    },
-    {
-        "Pergunta": "O tomate é um(a)…",
-        "Opções": ["Fruta", "Verdura", "Legume", "Raiz"],
-        "Resposta": "Fruta",
-    },
-    {
-        "Pergunta": "Qual é o nome do primeiro presidente negro eleito nos Estados Unidos?",
-        "Opções": ["George W. Bush", "Bill Clinton", "Barack Obama", "Donald Trump"],
-        "Resposta": "Barack Obama",
-    },
-    {
-        "Pergunta": "Quantos dias há em 3 semanas?",
-        "Opções": ["14", "18", "21", "28"],
-        "Resposta": "21",
-    },
-    {
-        "Pergunta": "O que as abelhas fabricam?",
-        "Opções": ["Seda", "Mel", "Pólen", "Algodão"],
-        "Resposta": "Mel",
-    },
-    {
-        "Pergunta": "A metade de 2 x 8 é igual a?",
-        "Opções": ["12", "16", "8", "10"],
-        "Resposta": "8",
-    },
-    {
-        "Pergunta": "Qual é o animal que mata mais humanos por ano?",
-        "Opções": ["Tubarão", "Crocodilo", "Mosquito", "Cobra"],
-        "Resposta": "Mosquito",
-    },
-    {
-        "Pergunta": "Um quilômetro equivale a quantos metros?",
-        "Opções": ["60 metros", "100 metros", "1000 metros", "10000 metros"],
-        "Resposta": "1000 metros",
-    },
-    {
-        "Pergunta": "O azeite de oliva é feito de quê?",
-        "Opções": ["Azeitona", "Milho", "Canola", "Oliveira"],
-        "Resposta": "Azeitona",
-    },
-    {
-        "Pergunta": "Como se chama a passagem da água do estado líquido para o sólido?",
-        "Opções": ["Solidificação", "Vaporização", "Condensação", "Fusão"],
-        "Resposta": "Solidificação",
-    },
-    {
-        "Pergunta": "Qual é o nome da substância responsável pela cor verde das plantas?",
-        "Opções": ["Melanina", "Hemoglobina", "Clorofila", "Antocianina"],
-        "Resposta": "Clorofila",
-    },
-    {
-        "Pergunta": "A música 'Livre estou' faz parte da trilha sonora de qual desenho animado?",
-        "Opções": [
-            "Frozen – Uma Aventura Congelante",
-            "O Rei Leão",
-            "Moana – Um Mar de Aventuras",
-            "Enrolados",
-        ],
-        "Resposta": "Frozen – Uma Aventura Congelante",
-    },
-    {
-        "Pergunta": "Complete o ditado: 'Quando a esmola é grande, o santo…'",
-        "Opções": ["foge.", "desconfia.", "pede mais.", "se aproxima,"],
-        "Resposta": "desconfia.",
-    },
-    {
-        "Pergunta": "Qual é o nome do rio que corta a cidade de Paris?",
-        "Opções": ["Tamisa", "Sena", "Rio de Janeiro", "Danúbio"],
-        "Resposta": "Sena",
-    },
+    # {
+    #     "Pergunta": "Qual é o único país onde o animal coala vive?",
+    #     "Opções": ["Brasil", "Estados Unidos", "Canadá", "Austrália"],
+    #     "Resposta": "Austrália",
+    # },
+    # {
+    #     "Pergunta": "As cores vermelho e azul misturadas formam qual cor?",
+    #     "Opções": ["Roxo", "Amarelo", "Verde", "Laranja"],
+    #     "Resposta": "Roxo",
+    # },
+    # {
+    #     "Pergunta": "Quantos anos tem um século?",
+    #     "Opções": ["100", "10", "1000", "50"],
+    #     "Resposta": "100",
+    # },
+    # {
+    #     "Pergunta": "Quem pintou o quadro Mona Lisa?",
+    #     "Opções": [
+    #         "Van Gogh",
+    #         "Osmar de Barros",
+    #         "Leonardo da Vinci",
+    #         "Leonardo DiCaprio",
+    #     ],
+    #     "Resposta": "Leonardo da Vinci",
+    # },
+    # {
+    #     "Pergunta": "Quem nasce em Pernambuco é o que?",
+    #     "Opções": ["Pernambuquês", "Pernambués", "Pernambucólico", "Pernambucano"],
+    #     "Resposta": "Pernambucano",
+    # },
+    # {
+    #     "Pergunta": "Que nome se dá para o coletivo de lobos?",
+    #     "Opções": ["Alcateia", "Cardume", "Vexame", "Matilha"],
+    #     "Resposta": "Alcateia",
+    # },
+    # {
+    #     "Pergunta": "Como é o nome da energia gerada pelo vento?",
+    #     "Opções": ["Hídrica", "Térmica", "Eólica", "Química"],
+    #     "Resposta": "Eólica",
+    # },
+    # {
+    #     "Pergunta": "Qual é o maior país da América do Sul?",
+    #     "Opções": ["Brasil", "Argentina", "Chile", "Colômbia"],
+    #     "Resposta": "Brasil",
+    # },
+    # {
+    #     "Pergunta": "Como é chamado o macho da cabra?",
+    #     "Opções": ["Cabrito", "Bode", "Carneiro", "Touro"],
+    #     "Resposta": "Bode",
+    # },
+    # {
+    #     "Pergunta": "Como é chamado o maior osso do corpo humano?",
+    #     "Opções": ["Fêmur", "Úmero", "Tíbia", "Costela"],
+    #     "Resposta": "Fêmur",
+    # },
+    # {
+    #     "Pergunta": "Qual é o sistema de governo adotado no Brasil?",
+    #     "Opções": [
+    #         "Monarquia parlamentarista",
+    #         "República presidencialista",
+    #         "Ditadura militar",
+    #         "República parlamentarista",
+    #     ],
+    #     "Resposta": "República presidencialista",
+    # },
+    # {
+    #     "Pergunta": "Qual é a floresta tropical mais extensa do mundo?",
+    #     "Opções": [
+    #         "Floresta Amazônica",
+    #         "Floresta Negra",
+    #         "Floresta de Sherwood",
+    #         "Floresta do Congo",
+    #     ],
+    #     "Resposta": "Floresta Amazônica",
+    # },
+    # {
+    #     "Pergunta": "Qual desses não é o nome de um oceano?",
+    #     "Opções": ["Polar", "Oceano Negro", "Pacífico", "Índico"],
+    #     "Resposta": "Oceano Negro",
+    # },
+    # {
+    #     "Pergunta": "Qual é a fórmula da água?",
+    #     "Opções": ["H2O", "CO2", "NaCl", "C6H12O6"],
+    #     "Resposta": "H2O",
+    # },
+    # {
+    #     "Pergunta": "Qual objeto costuma ser jogado pela noiva durante a festa de casamento?",
+    #     "Opções": ["Véu", "Sapato", "Aliança", "Buquê de flores"],
+    #     "Resposta": "Buquê de flores",
+    # },
+    # {
+    #     "Pergunta": "Qual é o nome do famoso navio que naufragou em 1912 e foi tema de um filme?",
+    #     "Opções": ["Queen Mary", "Enterprise", "Titanic", "Dear Rose"],
+    #     "Resposta": "Titanic",
+    # },
+    # {
+    #     "Pergunta": "Quantos dias tem um ano bissexto?",
+    #     "Opções": ["365", "366", "367", "364"],
+    #     "Resposta": "366",
+    # },
+    # {
+    #     "Pergunta": "Qual é o maior continente do mundo?",
+    #     "Opções": ["Europa", "América", "África", "Ásia"],
+    #     "Resposta": "Ásia",
+    # },
+    # {
+    #     "Pergunta": "Qual país tem como capital a cidade de Lisboa?",
+    #     "Opções": ["Espanha", "Portugal", "França", "Itália"],
+    #     "Resposta": "Portugal",
+    # },
+    # {
+    #     "Pergunta": "Quem é o responsável por buscar e devolver a bola ao campo durante um jogo de futebol?",
+    #     "Opções": ["Goleiro", "Zagueiro", "Atacante", "Gandula"],
+    #     "Resposta": "Gandula",
+    # },
+    # {
+    #     "Pergunta": "Você tem 36 laranjas e joga um terço fora, com quantas você fica?",
+    #     "Opções": ["6", "24", "12", "18"],
+    #     "Resposta": "24",
+    # },
+    # {
+    #     "Pergunta": "Quanto é 50% de 300?",
+    #     "Opções": ["25", "50", "100", "150"],
+    #     "Resposta": "150",
+    # },
+    # {
+    #     "Pergunta": "Qual é a capital da Espanha?",
+    #     "Opções": ["Valência", "Barcelona", "Sevilla", "Madrid"],
+    #     "Resposta": "Madrid",
+    # },
+    # {
+    #     "Pergunta": "Quantos séculos há em um milênio?",
+    #     "Opções": ["2 séculos", "10 séculos", "20 séculos", "15 séculos"],
+    #     "Resposta": "10 séculos",
+    # },
+    # {
+    #     "Pergunta": "Qual é o metal mais caro do mundo?",
+    #     "Opções": ["Ouro", "Platina", "Ródio", "Paládio"],
+    #     "Resposta": "Ródio",
+    # },
+    # {
+    #     "Pergunta": "Qual é a cor do sangue arterial?",
+    #     "Opções": ["Vermelho", "Azul", "Roxo", "Preto"],
+    #     "Resposta": "Vermelho",
+    # },
+    # {
+    #     "Pergunta": "Qual é o nome da haste de madeira de extremidade achatada usada para remar?",
+    #     "Opções": ["Vela", "Timão", "Remo", "Leme"],
+    #     "Resposta": "Remo",
+    # },
+    # {
+    #     "Pergunta": "Qual animal é conhecido como o 'rei da selva'?",
+    #     "Opções": ["Leopardo", "Tigre", "Leão", "Onça"],
+    #     "Resposta": "Leão",
+    # },
+    # {
+    #     "Pergunta": "Qual é o mineral capaz de enfraquecer o Super-Homem?",
+    #     "Opções": ["Ametista", "Quartzo", "Diamante", "Kriptonita"],
+    #     "Resposta": "Kriptonita",
+    # },
+    # {
+    #     "Pergunta": "Qual é o coletivo de lobos?",
+    #     "Opções": ["Matilha", "Bando", "Alcateia", "Manada"],
+    #     "Resposta": "Alcateia",
+    # },
+    # {
+    #     "Pergunta": "A moranga é uma variedade de:",
+    #     "Opções": ["Morango", "Abóbora", "Melancia", "Ameixa"],
+    #     "Resposta": "Abóbora",
+    # },
+    # {
+    #     "Pergunta": "Como é chamado um ângulo de 90º?",
+    #     "Opções": ["Reto", "Obtuso", "Agudo", "Raso"],
+    #     "Resposta": "Reto",
+    # },
+    # {
+    #     "Pergunta": "O tomate é um(a)…",
+    #     "Opções": ["Fruta", "Verdura", "Legume", "Raiz"],
+    #     "Resposta": "Fruta",
+    # },
+    # {
+    #     "Pergunta": "Qual é o nome do primeiro presidente negro eleito nos Estados Unidos?",
+    #     "Opções": ["George W. Bush", "Bill Clinton", "Barack Obama", "Donald Trump"],
+    #     "Resposta": "Barack Obama",
+    # },
+    # {
+    #     "Pergunta": "Quantos dias há em 3 semanas?",
+    #     "Opções": ["14", "18", "21", "28"],
+    #     "Resposta": "21",
+    # },
+    # {
+    #     "Pergunta": "O que as abelhas fabricam?",
+    #     "Opções": ["Seda", "Mel", "Pólen", "Algodão"],
+    #     "Resposta": "Mel",
+    # },
+    # {
+    #     "Pergunta": "A metade de 2 x 8 é igual a?",
+    #     "Opções": ["12", "16", "8", "10"],
+    #     "Resposta": "8",
+    # },
+    # {
+    #     "Pergunta": "Qual é o animal que mata mais humanos por ano?",
+    #     "Opções": ["Tubarão", "Crocodilo", "Mosquito", "Cobra"],
+    #     "Resposta": "Mosquito",
+    # },
+    # {
+    #     "Pergunta": "Um quilômetro equivale a quantos metros?",
+    #     "Opções": ["60 metros", "100 metros", "1000 metros", "10000 metros"],
+    #     "Resposta": "1000 metros",
+    # },
+    # {
+    #     "Pergunta": "O azeite de oliva é feito de quê?",
+    #     "Opções": ["Azeitona", "Milho", "Canola", "Oliveira"],
+    #     "Resposta": "Azeitona",
+    # },
+    # {
+    #     "Pergunta": "Como se chama a passagem da água do estado líquido para o sólido?",
+    #     "Opções": ["Solidificação", "Vaporização", "Condensação", "Fusão"],
+    #     "Resposta": "Solidificação",
+    # },
+    # {
+    #     "Pergunta": "Qual é o nome da substância responsável pela cor verde das plantas?",
+    #     "Opções": ["Melanina", "Hemoglobina", "Clorofila", "Antocianina"],
+    #     "Resposta": "Clorofila",
+    # },
+    # {
+    #     "Pergunta": "A música 'Livre estou' faz parte da trilha sonora de qual desenho animado?",
+    #     "Opções": [
+    #         "Frozen – Uma Aventura Congelante",
+    #         "O Rei Leão",
+    #         "Moana – Um Mar de Aventuras",
+    #         "Enrolados",
+    #     ],
+    #     "Resposta": "Frozen – Uma Aventura Congelante",
+    # },
+    # {
+    #     "Pergunta": "Complete o ditado: 'Quando a esmola é grande, o santo…'",
+    #     "Opções": ["foge.", "desconfia.", "pede mais.", "se aproxima,"],
+    #     "Resposta": "desconfia.",
+    # },
+    # {
+    #     "Pergunta": "Qual é o nome do rio que corta a cidade de Paris?",
+    #     "Opções": ["Tamisa", "Sena", "Rio de Janeiro", "Danúbio"],
+    #     "Resposta": "Sena",
+    # },
 ]
 
 
@@ -281,7 +281,7 @@ def show_question(idx_current_question, current_question):
     print(f"Pergunta {idx_current_question + 1}: {current_question["Pergunta"]}")
 
 
-def show_option(current_question):
+def show_options(current_question):
     current_options = current_question["Opções"]
     for idx_option, option in enumerate(current_options):
         print(f"{idx_option + 1}) {option}")
@@ -289,6 +289,7 @@ def show_option(current_question):
 
 def get_answer(current_question):
     user_choice = input("Digite a opção correta: ")
+    print()
     if user_choice.isdigit():
         user_choice_value = current_question["Opções"][int(user_choice) - 1]
         return user_choice_value
@@ -296,13 +297,26 @@ def get_answer(current_question):
         return None
 
 
+def validate_answer(user_choice_value, current_question):
+    if user_choice_value == current_question["Resposta"]:
+        return True
+
+
 def quiz(questions):
+    correct_answers = 0
     for idx_current_question, current_question in enumerate(questions):
         show_question(idx_current_question, current_question)
 
-        show_option(current_question)
+        show_options(current_question)
 
-        get_answer(current_question)
+        user_choice_value = get_answer(current_question)
+
+        if validate_answer(user_choice_value, current_question):
+            correct_answers += 1
+
+    print(
+        f"Você acertou {correct_answers} respostas de um total de {len(questions)} possíveis"
+    )
 
 
 quiz(questions)
